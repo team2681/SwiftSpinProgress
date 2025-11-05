739be59dd1c1348e850ceb2eadcafd9a66b06623
ANIL KUMAR <anilkumar2681@gmail.com>
2025-11-06 02:42:48 +0530

    Added LICENSE.txt

--- SwiftSpinProgress/build.gradle.kts
+++ SwiftSpinProgress/build.gradle.kts
@@ -63,3 +63,41 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
+mavenPublishing {
+
+    coordinates("io.github.team2681", "swiftspinprogress", "1.0.0")
+
+    pom {
+        name.set(project.properties["POM_NAME"] as String?)
+        description.set(project.properties["POM_DESCRIPTION"] as String?)
+        url.set(project.properties["POM_URL"] as String?)
+        licenses {
+            license()
+            {
+                name.set(project.properties["POM_LICENSE_NAME"] as String?)
+                url.set(project.properties["POM_LICENSE_URL"] as String?)
+            }
+        }
+        developers {
+            developer {
+                id.set(project.properties["POM_DEVELOPER_ID"] as String?)
+                name.set(project.properties["POM_DEVELOPER_NAME"] as String?)
+                email.set(project.properties["POM_DEVELOPER_EMAIL"] as String?)
+                url.set(project.properties["POM_DEVELOPER_URL"] as String?)
+            }
+        }
+        scm {
+            connection.set(project.properties["POM_SCM_URL"] as String?)
+            developerConnection.set(project.properties["POM_SCM_DEV_CONNECTION"] as String?)
+            url.set(project.properties["POM_SCM_CONNECTION"] as String?)
+        }
+    }
+}
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
+}
+
+tasks.withType<PublishToMavenRepository>().configureEach {
+    dependsOn(tasks.withType<Sign>())
+}
